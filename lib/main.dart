eab742b1a63c9de3e02bc7d161ae03ac89935942
omaryakout <omaryakout44@gmail.com>
2023-08-13 21:08:47 +0300

    change

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,18 @@ import 'models/expense_model.dart';
 import 'widgets/expenses.dart';
 
 import 'widgets/expenses_list/expense_item.dart';
+var kColorScheme = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 105, 69, 234))
 
 void main() {
   
   runApp(MaterialApp(
-    theme: ThemeData(useMaterial3: true),
+    theme: ThemeData().copyWith(useMaterial3: true,
+    colorScheme: kColorScheme,
+    appBarTheme: AppBarTheme().copyWith(
+      backgroundColor: kColorScheme.onSecondary,
+      foregroundColor:kColorScheme.onPrimary ,
+    ).
+    ),
     home: Expenses(),
   ));
 }
